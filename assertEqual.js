3c9c7c19222d90ea0edfd78e36cc4487da11ba2a
Rose Francis <rosennabs@gmail.com>
2023-06-15 18:14:46 -0600

    Implementing assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("⛔️⛔️⛔️ Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Hello", "Hello");
+assertEqual("Hello", "Hi");
+assertEqual(20, 32);
