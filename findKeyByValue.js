86c9aab755a3dc5888f09e269baeafff8d74954d
Rose Francis <rosennabs@gmail.com>
2023-06-21 21:10:35 -0600

    implement findKeyByValue

---
+++ findKeyByValue.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⛔️⛔️⛔️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//Scan the object and return the first key containing the given value
+const findKeyByValue = function (object1, value) {
+  const genre = Object.keys(object1);
+  for (string of genre) {
+    if (object1[string] === value) {
+      return string;
+    } 
+  } return undefined;
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+  action: "Fast and Furious"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Fast and Furious"), "action");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Fast Five"), undefined);
