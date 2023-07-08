4407f762a186a7e296d21319c26a3f87161d02ba
Rose Francis <rosennabs@gmail.com>
2023-07-07 22:19:12 -0600

    Updated the middleTest using mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,18 +1,3 @@
-// const assertEqual = require('../assertEqual');
-// const tail = require('../tail');
-
-// // Test Code: 
-// const result2 = tail(["Hello", "Rose", "Hi", "World"]);
-// assertEqual(result2.length, 3); 
-// assertEqual(result2[0], "Rose"); 
-// assertEqual(result2[1], "Hi"); 
-// assertEqual(result2[2], "World"); 
-
-// // Test Case: Check the original array 
-// const words = ["Hello", "Rose", "Hi", "World"];
-// tail(words);
-// assertEqual(words.length, 4);
-
 const assert = require('chai').assert;
 const tail = require('../tail');
 
